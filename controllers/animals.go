b132346b025b88db53059938dc4944d697185758
Gerry_Aballa <gerry.o.aballa@gmail.com>
2023-10-15 17:50:55 +0300

    Added controllers

---
+++ controllers/animals.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetAnimals(c *gin.Context) {
+	//TODO: implement this method to return all animals in the database
+}
+
+func GetAnimalType(c *gin.Context) {
+	//TODO: implement this method to get animal by type from the database
+}
+
+func GetAnimalName(c *gin.Context) {
+	//TODO: implement this method to get an animal by name from the database
+}
+
+func CreateAnimal(c *gin.Context) {
+	//TODO: implement this method to create a new animal in the database
+}
+
+func DeleteAnimal(c *gin.Context) {
+	//TODO: implement this method to delete an animal from the database
+}
